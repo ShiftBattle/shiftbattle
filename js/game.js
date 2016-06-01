357b790a1c5e4ea1d705ee0773e1698431f0bba2
jbain1 <jbainschool@gmail.com>
2016-06-01 20:56:50 +0000

    Name displays

--- js/game.js
+++ js/game.js
@@ -1,10 +1,10 @@
 /* global Eureca, Phaser, Player populatePowerUps, playerSpawns, randomize*/
 
-var myId=0;
+var myId = 0;
 
 var map;
 var walls;
-	
+
 var localPlayerSprite;
 var localPlayer;
 var playersList = {};
@@ -18,197 +18,277 @@ var keys;
 
 var powerUpsActive;
 
-function updatePlayerState(id, state){
+function updatePlayerState(id, state) {
 	// {console.log(state)
-		if (playersList[id] && id != myId)  {
+	if (playersList[id] && id != myId) {
 
-			playersList[id].cursor = state;
-			playersList[id].playerSprite.x = state.x;
-			playersList[id].playerSprite.y = state.y;
-			playersList[id].playerSprite.angle = state.angle;
-			playersList[id].playerSprite.rotation = state.rot;
+		playersList[id].cursor = state;
+		playersList[id].playerSprite.x = state.x;
+		playersList[id].playerSprite.y = state.y;
+		playersList[id].playerSprite.angle = state.angle;
+		playersList[id].playerSprite.rotation = state.rot;
 
-			playersList[id].update();
-		}
-		
+		playersList[id].update();
 	}
 
-function updatePowerups(newPowerups){
-		newPowerups.forEach(function(pu, idx) {
-			var powerUpSprite = game.powerUps.children[idx];
-			powerUpSprite.reset(pu[0][0], pu[0][1]);
-			powerUpSprite.type = pu[1];
-			powerUpSprite.play(pu[1]);
-		});
-	}
-	
-function updatePlayerHealth(playerShotandShooter){
+}
+
+function updatePowerups(newPowerups) {
+	newPowerups.forEach(function(pu, idx) {
+		var powerUpSprite = game.powerUps.children[idx];
+		powerUpSprite.reset(pu[0][0], pu[0][1]);
+		powerUpSprite.type = pu[1];
+		powerUpSprite.play(pu[1]);
+	});
+}
+
+function updatePlayerHealth(playerShotandShooter) {
 	var victim = playerShotandShooter[Object.keys(playerShotandShooter)[0]]
+	var shooter = playerShotandShooter[Object.keys(playerShotandShooter)[1]]
 	if (playersList[victim].shield.health > 0) {
-    	playersList[victim].shield.health--;
-    }
-    else {
-    	console.log(playersList[victim].shield.health, 'inside else statement on damage');
-    	playersList[victim].shield.kill();
-    	playersList[victim].cursor.health--;
-    	playersList[victim].cursor.shield = false;
-    }
-    if (playersList[victim].cursor.health <= 0) {
-       
-        playersList[victim].shield.kill();
-        playersList[victim].healthbar.kill();
-        playersList[victim].label.kill();
-        playersList[victim].playerSprite.kill();
-        playersList[victim].cursor.alive = false;
-        playersList[victim].cursor.exists = false;
-        playersList[victim].cursor.visible = false;
-        playersList[victim].cursor.shield = false;
+		playersList[victim].shield.health--;
+	}
+	else {
+		console.log(playersList[victim].shield.health, 'inside else statement on damage');
+		playersList[victim].shield.kill();
+		playersList[victim].cursor.health--;
+		playersList[victim].cursor.shield = false;
+	}
+	if (playersList[victim].cursor.health <= 0) {
 
+		playersList[victim].shield.kill();
+		playersList[victim].healthbar.kill();
+		playersList[victim].label.kill();
+		playersList[victim].playerSprite.kill();
+		playersList[victim].cursor.alive = false;
+		playersList[victim].cursor.exists = false;
+		playersList[victim].cursor.visible = false;
+		playersList[victim].cursor.shield = false;
+		for (var each in playersList) {
+			console.log(playersList[each].playerSprite.id, victim, "THY SHIT BE HERE")
+			if (playersList[each].playerSprite.id === victim && victim === myId) {
+				eurecaServer.killUpdate({
+					killer: shooter,
+					victim: victim
+				})
+			}
+		}
 		setTimeout(function() {
 			console.log("RESPAWN TIMEOUT FUNCTION");
-		
+
 			var loc = playerSpawns[randomize(playerSpawns)];
 			playersList[victim].playerSprite.x = loc[0];
 			playersList[victim].playerSprite.y = loc[1];
 			playersList[victim].playerSprite.revive();
 			playersList[victim].label.revive();
 			playersList[victim].healthbar.revive();
-			
+
 			playersList[victim].cursor.alive = true;
 			playersList[victim].cursor.exists = true;
 			playersList[victim].cursor.visible = true;
 			playersList[victim].cursor.health = 10;
 			playersList[victim].cursor.skin = 'handgun';
-			
+
 		}, 5000);
-    
-        
-    }
-    
-    }
+
+
+	}
+
+}
 
 //this function will handle client communication with the server
 var eurecaClientSetup = function() {
 	//create an instance of eureca.io client
 	var eurecaClient = new Eureca.Client();
-	
-	eurecaClient.ready(function (proxy) {	
+
+	eurecaClient.ready(function(proxy) {
 		eurecaServer = proxy;
 	});
-	
+
 	//methods defined under "exports" namespace become available in the server side
 	eurecaClient.exports.nameChange = function(id, name) {
-		for(var player in playersList){
+		for (var player in playersList) {
 			playersList[id].label.destroy();
-			playersList[id].label = game.add.text(playersList[id].playerSprite.x, playersList[id].playerSprite.y, '' + name + '', { font: "14px Arial", fill: "#ffffff", align: "center" });
-			playersList[id].label.anchor.setTo(.5, -1.7);  
+			playersList[id].label = game.add.text(playersList[id].playerSprite.x, playersList[id].playerSprite.y, '' + name + '', {
+				font: "14px Arial",
+				fill: "#ffffff",
+				align: "center"
+			});
+			playersList[id].label.anchor.setTo(.5, -1.7);
 			playersList[id].playerSprite.bringToTop();
-			}
+		}
 	};
-	
+
 	eurecaClient.exports.setInfo = function(info) {
-		//create() is moved here to make sure nothing is created before unique id assignation
-		myId = info.id;
-		create();
-		updatePowerups(info.powerups);
-		console.log(info.powerups)
-		// call powerup display with info.powerups
-		eurecaServer.handshake(myId);
-		ready = true;
-	}
+			//create() is moved here to make sure nothing is created before unique id assignation
+			myId = info.id;
+			create();
+			updatePowerups(info.powerups);
+			console.log(info.powerups)
+				// call powerup display with info.powerups
+			eurecaServer.handshake(myId);
+			ready = true;
+		}
 		// this removes the player from the game on disconnect
-	eurecaClient.exports.kill = function(id) {	
+	eurecaClient.exports.kill = function(id) {
 		if (playersList[id]) {
 			playersList[id].destroy();
 			console.log('Removing ', id, playersList[id], " from the game");
 		}
 	};
+
+	eurecaClient.exports.printKillText = function(obj) {
+		console.log(obj)
+		var killer = obj.killer
+		var victim = obj.victim
+
+		for (var each in playersList) {
+			console.log(playersList[each].playerSprite.id, victim, "print kill text")
+			if (playersList[each].playerSprite.id === victim && victim === myId) {
+				var t = game.add.text(0, 0, "Fragged by " + killer, {
+					font: "20px Arial",
+					fill: "#ffffff",
+					align: "center"
+				});
+				t.fixedToCamera = true
+				setTimeout(function() {
+
+
+					t.destroy();
+				}, 2000)
+			}
+			if (playersList[each].playerSprite.id === killer && killer === myId) {
+				var t = game.add.text(0, 0, "You fragged " + victim, {
+					font: "20px Arial",
+					fill: "#ffffff",
+					align: "center"
+				});
+				t.fixedToCamera = true
+				setTimeout(function() {
+
+
+					t.destroy();
+				}, 2000)
+			}
+		}
+	}
+	eurecaClient.exports.displayScoreboard = function(res, id) {
+		console.log(res)
+		console.log(id, 'id');
+		console.log(myId, 'myId')
+		if (id === myId) {
+
+			console.log(res, 'res')
+			console.log(id, 'id')
+			var stuff = Object.keys(res)
+			var scoreboard = game.add.text(300, 300, stuff, {
+				font: "20px Arial",
+				fill: "#ffffff",
+				align: "center"
+			});
+			scoreboard.fixedToCamera = true
+			setTimeout(function() {
+
+
+				scoreboard.destroy();
+			}, 2000);
+		}
+	}
+
+
 	// sends the powerups that are currently active to all players and spawns a new one if one of them is picked up
 	eurecaClient.exports.updatePowerUps = updatePowerups;
 
-	eurecaClient.exports.spawnEnemy = function(i, x, y)
-	{
+	eurecaClient.exports.spawnEnemy = function(i, x, y) {
 		if (i === myId) return; //this is me
 
 		console.log('SPAWN', i);
 		var plyr = new Player(i, game, localPlayerSprite, x, y);
 		playersList[i] = plyr;
 		plyr.update();
-		};
+	};
 
 	eurecaClient.exports.updateState = updatePlayerState;
 	eurecaClient.exports.removePlayerHealth = updatePlayerHealth;
-	
+
 	eurecaClient.exports.killPowerups = function() {
-		game.powerUps.children.forEach(function(pu) {pu.kill()});
+		game.powerUps.children.forEach(function(pu) {
+			pu.kill()
+		});
 	};
 };
 
-var game = new Phaser.Game(1200, 800, Phaser.CANVAS, 'phaser-example', { preload: preload, create: eurecaClientSetup, update: update, render: render });
+var game = new Phaser.Game(1200, 800, Phaser.CANVAS, 'phaser-example', {
+	preload: preload,
+	create: eurecaClientSetup,
+	update: update,
+	render: render
+});
 // var game = new Phaser.Game(4000, 3000, Phaser.CANVAS, 'phaser-example', { preload: preload, create: eurecaClientSetup, update: update, render: render });
 // var game = new Phaser.Game(window.width, window.height, Phaser.CANVAS, 'phaser-example', { preload: preload, create: eurecaClientSetup, update: update, render: render });
 
 
-function preload () {
+function preload() {
 	game.load.tilemap('fixedmap', 'assets/fixedmap.json', null, Phaser.Tilemap.TILED_JSON);
-    game.load.image('desert32', 'assets/desert32.png');
-    game.load.image('wall32', 'assets/wall32.png');
-    game.load.image('actuallyfloor', 'assets/actuallyfloor.png');
+	game.load.image('desert32', 'assets/desert32.png');
+	game.load.image('wall32', 'assets/wall32.png');
+	game.load.image('actuallyfloor', 'assets/actuallyfloor.png');
 
-    game.load.spritesheet('final-player', 'assets/finalplayer.png', 150, 150);
-    
-    game.load.image('bullet', 'assets/bullet2.png');
-    
-    game.load.spritesheet('healthbar', 'assets/healthbarfinal.png', 74.9, 10);
+	game.load.spritesheet('final-player', 'assets/finalplayer.png', 150, 150);
+
+	game.load.image('bullet', 'assets/bullet2.png');
+
+	game.load.spritesheet('healthbar', 'assets/healthbarfinal.png', 74.9, 10);
 	game.load.spritesheet('powerups', 'assets/powerups.png', 75, 75);
 	game.load.spritesheet('shield', 'assets/shield.png', 130, 128);
 
 }
 
-function create () {
+function create() {
 	game.physics.startSystem(Phaser.Physics.ARCADE);
-	
+
 	map = game.add.tilemap('fixedmap');
-    walls = map.createLayer('Tile Layer 1');
-    map.addTilesetImage('desert32', 'desert32');
-    map.addTilesetImage('wall32', 'wall32');
-    map.addTilesetImage('actuallyfloor', 'actuallyfloor');
-    map.setCollision([1]);
+	walls = map.createLayer('Tile Layer 1');
+	map.addTilesetImage('desert32', 'desert32');
+	map.addTilesetImage('wall32', 'wall32');
+	map.addTilesetImage('actuallyfloor', 'actuallyfloor');
+	map.setCollision([1]);
 
 	walls.resizeWorld();
-	
+
 	map.fixedToCamera = true;
-	
-    playersList = {};
-	
+
+	playersList = {};
+
 	localPlayer = new Player(myId, game, localPlayerSprite);
 	playersList[myId] = localPlayer;
 	localPlayerSprite = localPlayer.playerSprite;
 	// localPlayerSprite.x=0;
 	// localPlayerSprite.y=0;
 
-    localPlayerSprite.bringToTop();
+	localPlayerSprite.bringToTop();
 
-    game.camera.follow(localPlayerSprite);
+	game.camera.follow(localPlayerSprite);
 
-	keys = { 
+	keys = {
 		up: game.input.keyboard.addKey(Phaser.Keyboard.W),
 		down: game.input.keyboard.addKey(Phaser.Keyboard.S),
 		left: game.input.keyboard.addKey(Phaser.Keyboard.A),
 		right: game.input.keyboard.addKey(Phaser.Keyboard.D),
+		tab: game.input.keyboard.addKey(Phaser.Keyboard.TAB)
 		// reload: game.input.keyboard.addKey(Phaser.Keyboard.R),
 		// key1: game.input.keyboard.addKey(Phaser.Keyboard.ONE),
 		// key2: game.input.keyboard.addKey(Phaser.Keyboard.TWO),
 		// key3: game.input.keyboard.addKey(Phaser.Keyboard.THREE)
 	};
-	
+
 	populatePowerUps();
 	// console.log(localPlayer.powerUps.children);
-	
+
 	game.scale.fullScreenScaleMode = Phaser.ScaleManager.SHOW_ALL;
-	
+
 	var fullscreen = game.input.keyboard.addKey(Phaser.Keyboard.ONE);
-	
+
 	fullscreen.onDown.add(gofull, this);
 
 }
@@ -216,24 +296,22 @@ function create () {
 
 function gofull() {
 
-    if (game.scale.isFullScreen)
-    {
-        game.scale.stopFullScreen();
-    }
-    else
-    {
-        game.scale.startFullScreen(false);
-    }
+	if (game.scale.isFullScreen) {
+		game.scale.stopFullScreen();
+	}
+	else {
+		game.scale.startFullScreen(false);
+	}
 
 }
 
 
-function update () {
+function update() {
 	//do not update if client not ready
 	if (!ready) return;
-	
+
 	game.stage.disableVisibilityChange = true;
-	
+
 	var input = {
 		left: keys.left.isDown,
 		right: keys.right.isDown,
@@ -243,12 +321,12 @@ function update () {
 		tx: game.input.x + game.camera.x,
 		ty: game.input.y + game.camera.y
 	};
-	
+
 	// if (keys.key1.isDown) {
 	// 	localPlayer.cursor.skin = 'handgun';
 	// 	console.log(game.powerUps.children);
 	// 	eurecaServer.handleKeys({skin: 'handgun'});
-		
+
 	// }
 	// else if (keys.key2.isDown){
 	// 	localPlayer.cursor.skin = 'shotgun';
@@ -259,10 +337,14 @@ function update () {
 	// 	eurecaServer.handleKeys({skin: 'rifle'});
 	// }
 
+	if (keys.tab.isDown) {
+		eurecaServer.scoreDisplay(localPlayerSprite.id);
+		console.log(localPlayerSprite)
+	}
 
 
 	localPlayerSprite.rotation = game.physics.arcade.angleToPointer(localPlayerSprite);
-    
+
 	var inputChanged = (
 		localPlayer.cursor.left != input.left ||
 		localPlayer.cursor.right != input.right ||
@@ -272,8 +354,8 @@ function update () {
 		localPlayer.cursor.rot != localPlayerSprite.rotation ||
 		localPlayer.cursor.alive != localPlayerSprite.alive
 	);
-	
-	if (inputChanged){
+
+	if (inputChanged) {
 		// send latest valid state to the server
 		input.x = localPlayerSprite.x;
 		input.y = localPlayerSprite.y;
@@ -286,38 +368,36 @@ function update () {
 		eurecaServer.handleKeys(input);
 		localPlayer.cursor = input;
 	}
-	
-	
-	// This loop checks who should be killed
-    for (var i in playersList) {
-        if (!playersList[i]) continue;
-        var curBullets = playersList[i].bullets;
-        var curPlayer = playersList[i].playerSprite;
-        for (var j in playersList)
-        {
-            if (!playersList[j]) continue;
-            if (j!=i) {
-                var targetPlayer = playersList[j].playerSprite;
-                game.physics.arcade.overlap(curBullets, targetPlayer, bulletHitPlayer, null, this);
-                game.physics.arcade.collide(curBullets, walls, bulletHitWall, null, this);
-            }
-        }
-    }
-    
-    // This loop updates all the players
-    for (var i  in playersList) {
-            if (playersList[i].alive)
-            {
-                playersList[i].update();
-            }           
-    }
 
-    game.physics.arcade.overlap(game.powerUps, localPlayerSprite, collectPowerup, null, this);
-    game.physics.arcade.collide(localPlayerSprite, walls);
+
+	// This loop checks who should be killed
+	for (var i in playersList) {
+		if (!playersList[i]) continue;
+		var curBullets = playersList[i].bullets;
+		var curPlayer = playersList[i].playerSprite;
+		for (var j in playersList) {
+			if (!playersList[j]) continue;
+			if (j != i) {
+				var targetPlayer = playersList[j].playerSprite;
+				game.physics.arcade.overlap(curBullets, targetPlayer, bulletHitPlayer, null, this);
+				game.physics.arcade.collide(curBullets, walls, bulletHitWall, null, this);
+			}
+		}
+	}
+
+	// This loop updates all the players
+	for (var i in playersList) {
+		if (playersList[i].alive) {
+			playersList[i].update();
+		}
+	}
+
+	game.physics.arcade.overlap(game.powerUps, localPlayerSprite, collectPowerup, null, this);
+	game.physics.arcade.collide(localPlayerSprite, walls);
 
 }
 
-function collectPowerup (player, powerup){
+function collectPowerup(player, powerup) {
 	powerup.kill();
 	if (powerup.type === 'shotgun') {
 		localPlayer.cursor.skin = 'shotgun';
@@ -346,26 +426,26 @@ function collectPowerup (player, powerup){
 		});
 	}
 
-    eurecaServer.pickupPowerUp();
-    
-    console.log(powerup)
-    console.log(player.id, "picked up a", powerup.type, "powerup!");
+	eurecaServer.pickupPowerUp();
+
+	console.log(powerup)
+	console.log(player.id, "picked up a", powerup.type, "powerup!");
 }
 
 
-function bulletHitWall (bullet) {
+function bulletHitWall(bullet) {
 	// console.log(bullet, 'bullet has hit the wall');
 	bullet.kill();
-    
+
 }
 
-function bulletHitPlayer (player, bullet) {
+function bulletHitPlayer(player, bullet) {
 	bullet.kill();
-	
+
 	if (localPlayer.playerSprite.id === player.id)
 		playersList[player.id].damage(bullet);
-	}
+}
 
-function render () {
+function render() {
 
 }
--- server.js
+++ server.js
@@ -1,94 +1,102 @@
-var express = require('express')
-  , app = express(app)
-  , server = require('http').createServer(app);
+var express = require('express'),
+    app = express(app),
+    server = require('http').createServer(app);
 // serve static files from the current directory
 app.use(express.static(__dirname));
 //we'll keep clients data here
 var clients = {};
 var updatedPowers = [];
+var totalKills = [];
 powerUpUpdate();
 
 //get EurecaServer class
 var EurecaServer = require('eureca.io').EurecaServer;
 //create an instance of EurecaServer
-var eurecaServer = new EurecaServer({allow:['killPowerups', 'setInfo', 'spawnEnemy', 'kill', 'updateState', 'updatePowerUps', 'nameChange', 'removePlayerHealth']});
+var eurecaServer = new EurecaServer({
+    allow: ['killPowerups', 'setInfo', 'spawnEnemy', 'kill', 'updateState', 'updatePowerUps',
+        'nameChange', 'removePlayerHealth', 'displayScoreboard', 'printKillText'
+    ]
+});
 //attach eureca.io to our http server
 eurecaServer.attach(server);
 //eureca.io provides events to detect clients connect/disconnect
 //detect client connection
-eurecaServer.onConnect(function (conn) {    
+eurecaServer.onConnect(function(conn) {
     console.log('New Client id=%s ', conn.id, conn.remoteAddress);
-    
+
     //the getClient method provide a proxy allowing us to call remote client functions
-    var remote = eurecaServer.getClient(conn.id);    
-    
+    var remote = eurecaServer.getClient(conn.id);
+
     //register the client
-    clients[conn.id] = {id:conn.id, remote:remote};
-    
+    clients[conn.id] = {
+        id: conn.id,
+        remote: remote
+    };
+
     //here we call setId (defined in the client side)
-    remote.setInfo({id: conn.id, powerups: updatedPowers});  
-    
+    remote.setInfo({
+        id: conn.id,
+        powerups: updatedPowers
+    });
+
 });
 //detect client disconnection
-eurecaServer.onDisconnect(function (conn) {    
+eurecaServer.onDisconnect(function(conn) {
     console.log('Client disconnected ', conn.id);
     var removeId = clients[conn.id].id;
     delete clients[conn.id];
-    
-    for (var c in clients)
-    {
+
+    for (var c in clients) {
         var remote = clients[c].remote;
         //here we call kill() method defined in the client side
         remote.kill(conn.id);
-    }   
+    }
 });
 
-eurecaServer.exports.handshake = function(connId)
-{
+eurecaServer.exports.handshake = function(connId) {
     var remote = clients[connId].remote;
-    
+
     // loop thru all players
-    for (var cc in clients)
-    {
-        
+    for (var cc in clients) {
+
         if (cc === connId) {
             continue;
         }
         //send latest known position
-        var x = clients[cc].laststate ? clients[cc].laststate.x:  0;
-        var y = clients[cc].laststate ? clients[cc].laststate.y:  0;
-        
+        var x = clients[cc].laststate ? clients[cc].laststate.x : 0;
+        var y = clients[cc].laststate ? clients[cc].laststate.y : 0;
+
         // send current player to new player
         remote.spawnEnemy(clients[cc].id, x, y);
-        
+
         // send new player spawn to current player
         clients[cc].remote.spawnEnemy(connId, 0, 0);
     }
 };
 
-function powerUpUpdate(){
-var locs = [
-		[2161, 2421],
-		[2145, 1360],
-		[1814, 1629],
-		[1694, 1169],
-		[670, 2043],
-		[1022, 2421],
-		[522, 1087],
-		[324, 325],
-		[889, 129],
-		[1578, 200],
-		[2261, 234],
-		[1296, 815]
-	];
-        var powerUpType = ['rifle', 'shotgun', 'health', 'shield'];
-        updatedPowers = [];
-        for (var i = 0; i < 3; i++) {
-            var pu = updatedPowers[i] = [
-                locs.splice( Math.floor(locs.length * Math.random()), 1)[0],
-                powerUpType[Math.floor(powerUpType.length * Math.random())]
-            ];
-        }
+function powerUpUpdate() {
+    var locs = [
+        [2161, 2421],
+        [2145, 1360],
+        [1814, 1629],
+        [1694, 1169],
+        [670, 2043],
+        [1022, 2421],
+        [522, 1087],
+        [324, 325],
+        [889, 129],
+        [1578, 200],
+        [2261, 234],
+        [1296, 815]
+    ];
+    var powerUpType = ['rifle', 'shotgun', 'health', 'shield'];
+    updatedPowers = [];
+    for (var i = 0; i < 3; i++) {
+        var pu = updatedPowers[i] = [
+            locs.splice(Math.floor(locs.length * Math.random()), 1)[0],
+            powerUpType[Math.floor(powerUpType.length * Math.random())]
+        ];
+    }
 
     for (var c in clients) {
         var remote = clients[c].remote;
@@ -105,7 +113,7 @@ eurecaServer.exports.assignName = function(id, name) {
 }
 
 
-eurecaServer.exports.bulletHitsPlayer = function(playerShotandShooter){
+eurecaServer.exports.bulletHitsPlayer = function(playerShotandShooter) {
     // console.log(playerShotandShooter.shooter, 'the player being shot')
     // console.log(playerShotandShooter.playerShot, 'the shooter')
     for (var c in clients) {
@@ -114,7 +122,7 @@ eurecaServer.exports.bulletHitsPlayer = function(playerShotandShooter){
     }
 }
 
-eurecaServer.exports.pickupPowerUp = function(){
+eurecaServer.exports.pickupPowerUp = function() {
     for (var c in clients) {
         var remote = clients[c].remote;
         remote.killPowerups();
@@ -122,20 +130,69 @@ eurecaServer.exports.pickupPowerUp = function(){
     console.log('SOMEONE PICKED UP A POWERUP');
     setTimeout(function() {
         powerUpUpdate();
-    }, 5000);    
+    }, 5000);
 };
 
-
-eurecaServer.exports.handleKeys = function (keys) {
+eurecaServer.exports.killUpdate = function(packge) {
+    console.log(totalKills, 'totalkills before push')
+    totalKills.push(packge) //packge is the latest kill event
+    console.log(packge, 'here is package')
+    console.log(totalKills, 'totalkills after push')
     var conn = this.connection;
     var updatedClient = clients[conn.id];
-    
-    for (var c in clients)
-    {
+
+    for (var c in clients) {
         var remote = clients[c].remote;
-        
+        remote.printKillText(packge);
+    }
+}
+
+
+
+eurecaServer.exports.scoreDisplay = function(id) { //obj = totalKills
+    console.log(totalKills, 'here is totalkills')
+    if (totalKills.length === 0) return;
+    var victims = []
+    totalKills.forEach(function(each) {
+        victims.push(each.victim)
+    })
+    var bodycount = {}
+    victims.forEach(function(i) {
+        bodycount[i] = (bodycount[i] || 0) + 1;
+    });
+
+
+
+    var killers = []
+    totalKills.forEach(function(each) {
+        killers.push(each.killer)
+    })
+    var score = {}
+    killers.forEach(function(i) {
+        score[i] = (score[i] || 0) + 1;
+    });
+
+
+        //Construct a nice string out of killers and body count once NAMES are in.
+
+
+    //we display to only the ID passed in the function to us
+    for (var c in clients) {
+        var remote = clients[c].remote;
+        remote.displayScoreboard(killers, id);
+    }
+}
+
+
+eurecaServer.exports.handleKeys = function(keys) {
+    var conn = this.connection;
+    var updatedClient = clients[conn.id];
+
+    for (var c in clients) {
+        var remote = clients[c].remote;
+
         remote.updateState(updatedClient.id, keys);
-        
+
         //keep last known state so we can send it to new connected clients
         clients[c].laststate = keys;
     }
